72418e2a54eaed7d1d6175b76018f140b4e07c9c
0xdothis <olagokesikiru06@gmail.com>
2025-10-24 16:24:09 +0100

    chore: update folders

--- frontend/src/components/landing/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as HeroSection } from './HeroSection';
-export { default as FeaturesGrid } from './FeaturesGrid';
-export { default as HowItWorksSection } from './HowItWorksSection';
-export { default as LiveEventsCarousel } from './LiveEventsCarousel';
-export { default as StatsSection } from './StatsSection';
--- frontend/src/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-// Export all custom Web3 hooks
-export { useFactory } from './useFactory';
-export { useEvents } from './useEvents';
-export { useTickets } from './useTickets';
-export { useWorkers } from './useWorkers';
-export { useSponsors } from './useSponsors';
